f87a615b7497cddc19ddda9bcd660b8e34fedf89
tolotrasam <tolotrasam@gmail.com>
2016-06-02 00:45:25 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php 
 echo 'hello from the other siiiiiiiide';
-require 'php-static-component/dropdownmenu.php';
- require 'php-static-component\nav.php';
   require 'php-static-component/droptype.php';
+require 'php-static-component/dropdownmenu.php';
+echo '<br>CP1';
+ require 'php-static-component/nav.php';
+
 ?>
